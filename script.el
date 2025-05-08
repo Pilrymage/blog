560c1863bd6c607f797a8c194c5ddfccabc95d9a
pilrymage <windows.pilrymage@gmail.com>
2025-05-08 10:36:12 +0800

    script.el 初步流程

---
+++ script.el
@@ -0,0 +1,76 @@
+(require 'ox-publish)
+
+(setq org-html-head-include-default-style nil)
+(setq my-org-publish-root "C:\\Users\\pilrymage\\scoop\\apps\\cwrsync\\6.3.0\\rsyncfiles\\blog")
+(defconst my-html-head "
+<link rel=\"stylesheet\" href=\"./style.css\" type=\"text/css\"/>
+<link rel=\"icon\" href=\"favicon.ico\" type=\"image/x-icon\">
+"
+  "`:html-head' for `org-publish'.")
+
+(defconst my-html-preamble "
+ <nav>
+  <ul>
+    <li><a href=\"./index.html\">Home</a></li>
+    <li><a href=\"./about.html\">About</a></li>
+    <!-- <li><a href=\"./rss.xml\">RSS</a></li> -->
+    <li><a href=\"https://github.com/Pilrymage/\">GitHub</a></li>
+  </ul>
+</nav>
+"
+  "`:html-preamble' for `org-publish'." )
+(defconst my-html-postamble "
+<p class=\"author\">Author: <a href=\"mailto:pilrymage@gmail.com\">%a</a></p>
+<p class=\"date\">Date: %d</p>
+<p class=\"license\">License: <a href=\"https://www.creativecommons.org/licenses/by-nc/4.0/deed.zh-hans\">CC BY-NC 4.0</a></p>
+<!--<script src=\"https://utteranc.es/client.js\" repo=\"Spike-Leung/taxodium\" issue-term=\"pathname\" theme=\"github-light\" crossorigin=\"anonymous\" async></script>-->
+"
+  "`:html-postamble' for `org-publish'.")
+(setq org-publish-project-alist
+      `(
+        ("my-blog"
+         ;; :force t
+         :base-directory ,(expand-file-name  "./src" my-org-publish-root)        ; 文件目录
+         :base-extension "org"
+         :publishing-directory "./public_html" ; 存放目录
+         :section-numbers nil                          ;显示章节编号
+         :with-toc t
+         :with-tags t
+         :time-stamp-file nil
+         :recursive t                          ;递归处理 org 文件
+         :publishing-function org-html-publish-to-html ;使用org-html生成HTML
+         :headline-levels 2                            ; HTML标题级别限制
+         :html-head ,my-html-head ; 自定义 HTML head 内容
+         :html-preamble ,my-html-preamble
+         :html-postamble ,my-html-postamble
+         :html-html5-fancy t
+         :auto-sitemap t
+         :sitemap-filename "index.org"
+         :sitemap-title "Pilrymage"
+         :sitemap-sort-files anti-chronologically
+         :author "Pilrymage"
+         :email "pilrymage@gmail.com"
+         )
+        ("my-blog-static"
+         :base-directory ,(expand-file-name  "./static" my-org-publish-root)
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :recursive t
+         :publishing-function org-publish-attachment
+         :publishing-directory "./public_html"
+         )
+        ("sitemap"
+         :base-directory ,(expand-file-name  "./src" my-org-publish-root)
+         :base-extension "org"
+         :publishing-directory "./public_html" ; 存放目录
+         :publishing-function org-html-publish-to-html ;使用org-html生成HTML
+         :time-stamp-file nil
+         :include ("index.org")
+         :exclude ".*"
+         :html-head ,my-html-head
+         :html-preamble nil
+         :html-postamble nil
+         )
+        ("my-blog-all" :components ("sitemap" "my-blog" "my-blog-static" ))
+        ))
+(org-publish-project "my-blog-all")
+(browse-url-default-browser (concat "file://" (expand-file-name "./public_html" my-org-publish-root)))
