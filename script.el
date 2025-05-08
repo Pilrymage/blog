71db60fdde115688e9e2878428549baf35456beb
pilrymage <windows.pilrymage@gmail.com>
2025-05-08 17:55:02 +0800

    feat: Able to insert some image

--- script.el
+++ script.el
@@ -3,7 +3,7 @@
 (setq org-html-head-include-default-style nil)
 (setq my-org-publish-root "C:\\Users\\pilrymage\\scoop\\apps\\cwrsync\\6.3.0\\rsyncfiles\\blog")
 (defconst my-html-head "
-<link rel=\"stylesheet\" href=\"./style.css\" type=\"text/css\"/>
+<link rel=\"stylesheet\" href=\"./static/style.css\" type=\"text/css\"/>
 <link rel=\"icon\" href=\"favicon.ico\" type=\"image/x-icon\">
 "
   "`:html-head' for `org-publish'.")
@@ -30,9 +30,9 @@
       `(
         ("my-blog"
          ;; :force t
-         :base-directory ,(expand-file-name  "./src" my-org-publish-root)        ; 文件目录
+         :base-directory ,(concat my-org-publish-root "\\src")        ; 文件目录
          :base-extension "org"
-         :publishing-directory "./docs" ; 存放目录
+         :publishing-directory ,(concat my-org-publish-root "\\docs") ; 存放目录
          :section-numbers nil                          ;显示章节编号
          :with-toc t
          :with-tags t
@@ -52,16 +52,16 @@
          :email "pilrymage@gmail.com"
          )
         ("my-blog-static"
-         :base-directory ,(expand-file-name  "./static" my-org-publish-root)
+         :base-directory ,(concat my-org-publish-root "\\src")
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
          :recursive t
          :publishing-function org-publish-attachment
-         :publishing-directory "./docs"
+         :publishing-directory ,(concat my-org-publish-root "\\docs")
          )
         ("sitemap"
-         :base-directory ,(expand-file-name  "./src" my-org-publish-root)
+         :base-directory ,(concat my-org-publish-root "\\src")
          :base-extension "org"
-         :publishing-directory "./docs" ; 存放目录
+         :publishing-directory ,(concat my-org-publish-root "\\docs") ; 存放目录
          :publishing-function org-html-publish-to-html ;使用org-html生成HTML
          :time-stamp-file nil
          :include ("index.org")
@@ -73,4 +73,4 @@
         ("my-blog-all" :components ("sitemap" "my-blog" "my-blog-static" ))
         ))
 (org-publish-project "my-blog-all")
-(browse-url-default-browser (concat "file://" (expand-file-name "./public_html" my-org-publish-root)))
+;; (browse-url-default-browser (concat "file://" (concat "./public_html" my-org-publish-root)))
